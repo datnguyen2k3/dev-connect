bd1e7cd3efa952c42fcd40096f5749b98fc700e0
Dat Nguyen <thanhdatmu2003@gmail.com>
2023-03-31 17:36:54 +0700

    fix css

--- api/urls.py
+++ api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # auth
     path("", include("api.app.devsearch_auth.urls")),
     path("projects/", include("api.app.projects.urls")),
+    path("users/", include("api.app.users.urls")),
 ]
