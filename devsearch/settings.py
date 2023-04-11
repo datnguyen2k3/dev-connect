e7d6e431b15d56c3b0b409b51868496fe60129b8
samoy <antalois23@gmail.com>
2023-04-11 18:43:35 +0700

    feat: migrate database to postgresql

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -124,10 +124,16 @@ WSGI_APPLICATION = "devsearch.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "devconnect",
+        "USER": "postgres",
+        "PASSWORD": "samoy",
+        "HOST": "localhost",
+        "PORT": "5432"
     }
 }
+
+
 
 
 # Password validation
